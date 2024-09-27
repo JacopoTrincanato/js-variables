ac6c8be287bb3d45204c16ae40fc639954d2a26c
JacopoTrincanato <trincanatoj@gmail.com>
2024-09-27 15:32:54 +0200

    solve point two strings

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -12,6 +12,12 @@ console.log(name);
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
 
+let number = 10;
+let text = 'Buongiorno a tutti';
+let bool = false;
+console.log(number, text, bool);
+
+
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
 // in console la stringa con il nome completo
